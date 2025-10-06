ca65596663713cd48f2456dc358899a6e31e7bcd
Leovigildo Joao <leovigildojoao902@gmail.com>
2025-10-06 01:39:03 +0100

    Change CORS on backend

--- backend/api/src/main/java/com/example/api/repositories/ProjectRepository.java
+++ backend/api/src/main/java/com/example/api/repositories/ProjectRepository.java
@@ -3,7 +3,6 @@ package com.example.api.repositories;
 import com.example.api.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.UUID;
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, UUID> {
+public interface ProjectRepository extends JpaRepository<Project, String> {
 }
